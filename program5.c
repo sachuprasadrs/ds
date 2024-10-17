987a91e73a9c7f46c77a24857dbd02e96e9401e7
sachuprasadrs <sachuprasadrs@gmail.com>
2024-10-17 09:54:35 +0530

    Create program5.c

---
+++ program5.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void push(struct Node** top, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *top; 
+    *top = newNode;       
+    printf("Pushed: %d\n", data);
+}
+
+int pop(struct Node** top) {
+    if (*top == NULL) {
+        printf("Stack is empty. Cannot pop.\n");
+        return -1; 
+    }
+    struct Node* temp = *top;
+    int poppedData = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    printf("Popped: %d\n", poppedData);
+    return poppedData;
+}
+
+void display(struct Node* top) {
+    if (top == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+    struct Node* current = top;
+    printf("Stack elements: ");
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* stack = NULL; 
+
+    int choice, value;
+
+    do {
+        printf("\nStack Operations:\n");
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the value to push: ");
+                scanf("%d", &value);
+                push(&stack, value);
+                break;
+            case 2:
+                pop(&stack);
+                break;
+            case 3:
+                display(stack);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 4);
+
+    while (stack != NULL) {
+        pop(&stack);
+    }
+
+    return 0;
+}
+
