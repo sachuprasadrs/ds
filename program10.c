23f8e8e94f432df5829f76a9ef664d046ac84f52
sachuprasadrs <sachuprasadrs@gmail.com>
2024-12-30 09:27:09 +0530

    Update program10.c

--- program10.c
+++ program10.c
@@ -1,47 +1,78 @@
 #include<stdio.h>
+#define max 100
 
-int a, b, u, v, n, i, j, ne = 1;
-int visited[10] = {0}, min, mincost = 0, cost[10][10];
+int graph[max][max];
+int nodes[max];
+int n, i, j;
 
-void main() {
-    printf("\nEnter the number of nodes: ");
-    scanf("%d", &n);
+void prims()
+{
+    int selected[max] = {0}; 
+    int edge_count = 0;      // Number of edges selected in MST
+    int total_cost = 0;      // Total cost of the MST
 
-    printf("\nEnter the adjacency matrix:\n");
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0) {
-                cost[i][j] = 999;  
-            }
-        }
-    }
-
-    visited[1] = 1;  
+    // Start from the first node
+    selected[0] = 1;
+ 
     printf("\nEdges in the Minimum Spanning Tree:\n");
 
-    while (ne < n) {  
-        min = 999;
-        for (i = 1; i <= n; i++) {
-            if (visited[i]) {
-                for (j = 1; j <= n; j++) {
-                    if (!visited[j] && cost[i][j] < min) {
-                        min = cost[i][j];
-                        a = u = i;
-                        b = v = j;
+    while (edge_count < n - 1)
+    {
+       int min = 1e9;
+        int x = 0, y = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            if (selected[i]) 
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (!selected[j] && graph[i][j]) 
+                    {
+                        if (graph[i][j] < min)
+                        {
+                            min = graph[i][j];
+                            x = i;
+                            y = j;
+                        }
                     }
                 }
             }
         }
 
-        if (!visited[v]) {
-            printf(" Edge %d: (%d -> %d) cost: %d\n", ne++, u, v, min);
-            mincost += min;
-            visited[v] = 1;
-        }
-
-        cost[a][b] = cost[b][a] = 999;  
+        printf("Edge %d -> %d (Weight: %d)\n", nodes[x], nodes[y], graph[x][y]);
+        total_cost += graph[x][y];
+        selected[y] = 1; 
+        edge_count++;
     }
 
-    printf("\nMinimum cost = %d\n", mincost);
+    printf("\nTotal cost of the Minimum Spanning Tree: %d\n", total_cost);
+}
+
+int main()
+{
+    printf("\n\t\t PRIM'S ALGORITHM \n\n");
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter the node values:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Node %d: ", i + 1);
+        scanf("%d", &nodes[i]);
+    }
+
+    printf("\nEnter the adjacency matrix (weights of edges, 0 for no edge):\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j =0; j < n; j++)
+        {
+            printf("Weight between %d and %d: ", nodes[i], nodes[j]);
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    prims();
+
+    return 0;
 }
